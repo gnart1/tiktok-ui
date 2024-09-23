f6a4d278b385ed4e6813471fb56159245af54e97
gnart1 <76728890+gnart1@users.noreply.github.com>
2024-09-23 20:19:46 +0700

    Cấu hình router layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout';
 function App() {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout === null ? Fragment : (route.layout || DefaultLayout);
+
+            const Page = route.component
+            return (
+              <Route
+                key={index} path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
